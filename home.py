97c424757a2af5af932519df6091f77bcd7e864b
Passatka <107476342+Passatka@users.noreply.github.com>
2024-06-02 20:29:43 +0300

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 home = Flask(__name__, template_folder="templates")
+
+high_scores = {'circle': 0} #Algselt võtab 0 high scoreks
 
 @home.route("/")
 def index():
@@ -14,6 +16,21 @@ def circle():
 @home.route("/symmetry")
 def symmetry():
     return render_template("symmetry.html")
+
+@home.route("/update_score", methods = ['POST'])
+def update_score():
+    data = request.get_json()
+    score = data['score']
+    
+    new_high_score = False
+    if score > high_scores['circle']:
+        high_scores['circle'] = score
+        new_high_score = True
+    
+    return jsonify({
+        'new_high_score': new_high_score,
+        'high_score': high_scores['circle']
+    })
 
 if __name__ == '__main__':
     home.run(debug=True)
